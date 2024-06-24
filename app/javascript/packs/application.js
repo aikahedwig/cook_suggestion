73f1aaf4d89b88657bee9389b95291a1598a3a8f
aikahed <aikahedwig687@gmail.com>
2024-06-24 20:09:11 +0900

    edit rais_admin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'app/assets/stylesheets/rails_admin.scss'
+import "rails_admin/src/rails_admin/base";
+// import 'app/assets/stylesheets/rails_admin.scss'
 
 Rails.start()
 Turbolinks.start()
